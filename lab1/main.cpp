6d158fdf3c581c56e939f032f4fe96a18813ef62
BuyWell <maksim-d2013@mail.ru>
2023-10-14 15:35:00 +0300

    add initiateSystem

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <string>
+#include <fstream>
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
@@ -10,7 +12,7 @@
 #define THREAD_COUNT 8
 
 int bodiesCount, timeSteps;
-double *masses, GravConstant;
+double GravConstant;
 
 class Point2D {
 public:
@@ -21,7 +23,7 @@ public:
 
     Point2D(double _x, double _y) : x(_x), y(_y) {}
 
-    void setNewData(double _x, double _y){
+    void setNewData(double _x, double _y) {
         this->x = _x;
         this->y = _y;
     }
@@ -35,6 +37,7 @@ public:
     int number;
     std::vector<double> forces;
     Point2D speed;
+
     Body(double x, double y, double Vx, double Vy, double m, int number) {
         this->point = Point2D(x, y);
         this->speed = Point2D(Vx, Vy);
@@ -43,16 +46,50 @@ public:
         this->forces.resize(bodiesCount);
         pthread_rwlock_init(&rwlock, nullptr);
     }
-    ~Body(){
+
+    ~Body() {
         pthread_rwlock_destroy(&rwlock);
     }
+
 private:
-    pthread_rwlock_t rwlock;
+    pthread_rwlock_t rwlock{};
 };
 
 std::vector<Body> bodies;
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+bool initiateSystem(const std::string &filename) {
+    std::ifstream inputFile(filename);
+    double m, x, y, Vx, Vy;
+
+    if (!inputFile) {
+        std::cout << "Could not open the file" << std::endl;
+        return false;
+    }
+
+    inputFile >> GravConstant >> bodiesCount >> timeSteps;
+    for (int i = 0; i < bodiesCount; i++) {
+        inputFile >> m;
+        inputFile >> x >> y;
+        inputFile >> Vx >> Vy;
+        bodies.emplace_back(x, y, Vx, Vy, m, i);
+    }
+
+    if (inputFile.fail()) {
+        std::cerr << "Could not read data from the file." << std::endl;
+        return false;
+    }
+
+    inputFile.close();
+    return true;
+}
+
+int main(int argC, char *argV[]) {
+    if (argC != 2)
+        printf("Usage : %s <file name containing system configuration data>", argV[0]);
+    else {
+        if (initiateSystem(argV[1])) {
+            std::cout << "Body   :     x              y           vx              vy   " << std::endl;
+        }
+        return 0;
+    }
 }
